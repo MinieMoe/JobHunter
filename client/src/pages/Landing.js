fab30b3d94da1f6d876c69e0b59f98fdadfe596c
MinieMoe <nguye384@wwu.edu>
2023-01-04 01:26:49 -0800

    Upload

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,15 +1,17 @@
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
-import styled from 'styled-components'
 import React from 'react'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Link } from 'react-router-dom'
+import { Logo } from '../components'
 
 const Landing = () => {
   return(
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className='logo'/>
+        <Logo/>
       </nav>
-      <div className='container_page'>
+      <div className='container page'>
         <div className='info'>
           <h1>
             job <span>tracking</span> app
@@ -17,51 +19,13 @@ const Landing = () => {
           <p>
             I just wanna get a job
           </p>
-          <button className='btn btn-hero'>
+          <Link to='/register' className='btn btn-hero'>
             Login/Register
-          </button>
+          </Link>
           <img src={main} alt="job hunt" className='img main-img'/>
         </div>
       </div>
     </Wrapper>
   )
 }
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`
 export default Landing;
